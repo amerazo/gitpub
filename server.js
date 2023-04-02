93494342fd986405be0b44697352bf9d7b80d8d0
secretivo <secretivo@secretivos-Air.lan>
2023-04-02 15:44:05 -0400

    styled home page, ensured all routes are okay and finalized food_show

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.set('view engine', 'ejs');
 
 //----------Welcome Route ----------------------------//
 //----------Welcome Route ----------------------------//
-app.get('/', (req, res) => {
-    res.render('Welcome to the Gitpub App');
-});
+app.get("/", (req, res)=>{
+    res.render("home_index")
+})
 
 //----------Drinks Route ----------------------------//
 //----------Drinks Route ----------------------------//
@@ -25,6 +25,19 @@ app.get("/drinks", (req, res)=>{
 app.get("/drinks/:id", (req, res)=>{
     let drink = drinks[req.params.id];
     res.render("drinks_show.ejs", {drink});
+})
+
+//----------FOOD Route --------------------------//
+//----------FOOD Route ---------------------- ---//
+app.get("/foods", (req, res)=>{
+    res.render("food_index", {foods}) 
+
+})
+//----------FOOD ID Route ---------------------- ---//
+//----------FOOD ID Route ---------------------- ---//
+app.get("/foods/:id", (req, res)=>{
+    let food = foods[req.params.id]
+    res.render("food_show", {food})
 })
 
 //-----------Listen for Server------------------------//
